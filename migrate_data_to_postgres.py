4186415dd4ad36b316532058a34a3626660bb9e3
NLSanyu <lydiansanyu@gmail.com>
2021-08-25 13:26:39 +0300

    Read data and split it into three csvs

--- migrate_data_to_postgres.py
+++ migrate_data_to_postgres.py
@@ -13,21 +13,9 @@ def remove_prefix(df, prefix):
     return df
 
 
-def prepare_data(share_events_df, event_properties_df):
-    # Add a name to event types that have no name and appear as links ("http...")
-    share_events_df.loc[
-        share_events_df["event_type"].str.startswith("http"), "event_type"
-    ] = "Share Show Room:Studio"
+def clean_data(events):
+    share_events_df = pd.json_normalize(events)
 
-    # rename Share Presentation:ContentAdmin to Share Presentation:Content Admin
-    share_events_df.event_type.replace(
-        "Share Presentation:ContentAdmin",
-        "Share Presentation:Content Admin",
-        inplace=True,
-    )
-
-
-def clean_data(share_events_df):
     # Separate data into different parts that represent tables in our data model
     # `event properties` data
     event_properties_df = share_events_df.loc[
@@ -73,13 +61,27 @@ def clean_data(share_events_df):
     share_events_df.drop(columns=new_cols_to_drop, inplace=True, errors="ignore")
     share_events_df.drop_duplicates(subset=["insert_id"], inplace=True)
 
-    # preparatory cleaning of the data before it can be graphed
-    prepare_data(share_events_df, event_properties_df)
+    # Add a name to event types that have no name and appear as links ("http...")
+    share_events_df.loc[
+        share_events_df["event_type"].str.startswith("http"), "event_type"
+    ] = "Share Show Room:Studio"
+
+    # rename Share Presentation:ContentAdmin to Share Presentation:Content Admin
+    share_events_df.event_type.replace(
+        "Share Presentation:ContentAdmin",
+        "Share Presentation:Content Admin",
+        inplace=True,
+    )
+
+    users_df.to_csv("users.csv")
+    share_events_df.to_csv("share_events.csv")
+    event_properties_df.to_csv("event_properties.csv")
 
     return users_df, share_events_df, event_properties_df
 
 
-def read_mongo_data(collection_name, date_limit):
+
+def read_mongo_data(collection_name):
     user = config("MONGO_USER")
     password = config("MONGO_PASSWORD")
 
@@ -87,15 +89,24 @@ def read_mongo_data(collection_name, date_limit):
         client = pymongo.MongoClient(f"mongodb+srv://{user}:{password}@cluster0.yhpvw.mongodb.net/masterwizr-data-db?retryWrites=true&w=majority")
         db = client["masterwizr-data-db"]
         logging.info("Connected to Mongo")
-        collection = db["collection_name"]
-        records = collection.find()
+        collection = db[collection_name]
+        events = list(collection.find())
+
     except Exception as e:
         logging.error(traceback.print_exc())
-        return {"statusCode": 500, "body": {"message": e.message}}
+        return {"statusCode": 500, "body": {"message": "Error connecting to MongoDB"}}
 
-    final_data = clean_data(records)
-    sql_insert(final_data)
+    return events
 
 
-def sql_insert(data):
+def sql_insert(users_df, share_events_df, event_properties_df):
     pass
+
+
+if __name__ == "__main__":
+    prod = read_mongo_data("production")
+    users_df, share_events_df, event_properties_df = clean_data(prod)
+    sql_insert(users_df, share_events_df, event_properties_df)
+
+    # read_mongo_data("staging")
+    # read_mongo_data("beta")
